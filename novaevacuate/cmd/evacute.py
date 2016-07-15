29aea193330cb74242eee8bb08ad078d5d1bcbd4
fabian4 <cybing4@gmail.com>
2016-07-15 15:06:59 +0800

    evacuate start update
    Signed-off-by: fabian <cybing4@gmail.com>

--- novaevacuate/cmd/evacute.py
+++ novaevacuate/cmd/evacute.py
@@ -7,14 +7,15 @@ import time
 class MyDaemon(object):
     def __init__(self):
         self.stdin_path = '/dev/null'
-	self.stdout_path = '/dev/null'
-	self.stderr_path = '/dev/null'
-	self.pidfile_path = '/tmp/nova_evacuate.pid'
-	self.pidfile_timeout = 5
+        self.stdout_path = '/dev/null'
+        self.stderr_path = '/dev/null'
+        self.pidfile_path = '/tmp/nova_evacuate.pid'
+        self.pidfile_timeout = 5
+
     def run(self):
         while True:
             manage.manager()
-	    time.sleep(30)
+        time.sleep(30)
 
 my_daemon = MyDaemon()
 daemon_runner = runner.DaemonRunner(my_daemon)
