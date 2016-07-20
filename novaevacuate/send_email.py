24c8a0e8e0556765d98e5eaec5ece3f97a0dcccb
zhouyang1222 <419606291@qq.com>
2016-07-20 14:06:46 +0800

    add file send_email.py
    zhouyang <yang.zhou1222@gmail.com>

---
+++ novaevacuate/send_email.py
@@ -0,0 +1,18 @@
+import smtplib
+from email.mime.text import MIMEText
+
+class Email(object):
+    def __init__(self):
+        self._user = "yang.zhou@eayun.com"
+        self._pwd  = "******"
+        self._to   = "419606291@qq.com"
+
+    def send_email(self, message):
+        msg = MIMEText(message)
+        msg["Subject"] = "mail from: eayunstack"
+        msg["From"]    = self._user
+        msg["To"]      = self._to
+        s = smtplib.SMTP("smtp.exmail.qq.com", timeout=30)
+        s.login(self._user, self._pwd)
+        s.sendmail(self._user, self._to, msg.as_string())
+        s.close()
